79d792bbca6921a4d80433a709bfdaf9a967e6ad
palm <tauman.exe@gmail.com>
2023-05-08 16:34:56 +0200

    misc

--- Assets/src/ProcGen/TilePerlinNoise.cs
+++ Assets/src/ProcGen/TilePerlinNoise.cs
@@ -15,22 +15,16 @@ public class TilePerlinNoise : MonoBehaviour
     {
         float rx = Random.value * 10000f;
         float ry = Random.value * 10000f;
-        int low = 0;
-        int high = 0;
+
         for (int x = -size.x; x < size.x; x++)
         {
             for (int y = -size.y; y < size.y; y++)
             {
                 var localValue = .8f * Mathf.PerlinNoise(x * .2f + rx + y * .03f, y * .2f + ry + x * .03f) + Random.value * .25f;
                 int goodValue = Mathf.Clamp(Mathf.FloorToInt(localValue * Tiles.Length), 0, Tiles.Length - 1);
-                if (goodValue == 0)
-                    low++;
-                if (goodValue == 1)
-                    high++;
                 Tilemap.SetTile(new Vector3Int(x, y, 0),  Tiles[goodValue]);
             }
         }
-        Debug.Log($"Bright: {low}, Dark: {high}");
     }
 
 }
