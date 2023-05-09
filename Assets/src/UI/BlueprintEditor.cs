7871a3c096921a11c717ddc2b87195a3a2ef9bf0
palm <tauman.exe@gmail.com>
2023-05-09 13:32:52 +0200

    Made the Blueprint wall editing slightly more predictable, hopefully.

--- Assets/src/UI/BlueprintEditor.cs
+++ Assets/src/UI/BlueprintEditor.cs
@@ -6,16 +6,13 @@ using UnityEngine.Tilemaps;
 public class BlueprintEditor : MonoBehaviour
 {
     public Blueprint blueprint;
-    public TileBase WallTile;
-
+    [SerializeField]
+    public TileBase Sample;
+    public TileBase DeleteTile;
     public NetInput NetInput;
 
-    int currentTile;
-
-    void Start()
-    {
-        currentTile = TileCollection.Instance.IndexOf(WallTile);
-    }
+    int SampleIndex => TileCollection.Instance.IndexOf(Sample);
+    int DeleteIndex => TileCollection.Instance.IndexOf(DeleteTile);
 
     // Update is called once per frame
     void Update()
@@ -24,11 +21,31 @@ public class BlueprintEditor : MonoBehaviour
         {
             var clickPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             // we clicked!
-            var tile = blueprint.tilemap.GetTile(clickPosition);
-            if (tile == null)
-                NetInput.SetBlueprint(blueprint.tilemap.WorldToCell(clickPosition), currentTile);
-            else
-                NetInput.SetBlueprint(blueprint.tilemap.WorldToCell(clickPosition), -1);
+            var blueprintTile = blueprint.tilemap.GetTile(clickPosition);
+            var realTile = GridManager.instance.Walls.GetTile(clickPosition);
+            if(realTile == Sample)
+            {
+                if(blueprintTile == null)
+                    NetInput.SetBlueprint(blueprint.tilemap.WorldToCell(clickPosition), DeleteIndex);
+                else
+                    NetInput.SetBlueprint(blueprint.tilemap.WorldToCell(clickPosition), -1);
+            }
+            else if(realTile == null)
+            {
+                if(blueprintTile == Sample)
+                    NetInput.SetBlueprint(blueprint.tilemap.WorldToCell(clickPosition), -1);
+                else
+                    NetInput.SetBlueprint(blueprint.tilemap.WorldToCell(clickPosition), SampleIndex);
+            }
+            else // different tile
+            {
+                if(blueprintTile == DeleteTile)
+                    NetInput.SetBlueprint(blueprint.tilemap.WorldToCell(clickPosition), -1);
+                else if(blueprintTile == Sample)
+                    NetInput.SetBlueprint(blueprint.tilemap.WorldToCell(clickPosition), DeleteIndex);
+                else
+                    NetInput.SetBlueprint(blueprint.tilemap.WorldToCell(clickPosition), SampleIndex);
+            }
         }
     }
 }
