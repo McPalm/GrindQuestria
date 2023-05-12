0f0795c43ec279219195249688fb6c186ad43646
palm <tauman.exe@gmail.com>
2023-05-12 03:48:24 +0200

    Rejigged the Tiles a tad.

--- Assets/src/BuildingTile.cs
+++ Assets/src/BuildingTile.cs
@@ -6,5 +6,20 @@ using UnityEngine.Tilemaps;
 [CreateAssetMenu]
 public class BuildingTile : Tile
 {
+    public Sprite[] sprites;
+
     public Building building;
+
+    public override void GetTileData(Vector3Int position, ITilemap tilemap, ref TileData tileData)
+    {
+        base.GetTileData(position, tilemap, ref tileData);
+        if(sprites.Length > 0)
+        {
+            tileData.sprite = sprites[(999999+position.x + position.y)%sprites.Length];
+        }
+        var m = tileData.transform;
+        m.SetTRS(Vector3.zero, Quaternion.identity, Vector3.one);
+        tileData.transform = m;
+
+    }
 }
