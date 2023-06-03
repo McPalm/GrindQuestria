8cabe69fe85f9ef25d93d42a040cccf64e485ccd
palm <tauman.exe@gmail.com>
2023-06-03 17:27:37 +0200

    UI element to show aquired items in the bottom right corner. Probably works, who knows really, need to actually hook it up with events.

---
+++ Assets/src/UI/ItemGetPopupUI.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemGetPopupUI : MonoBehaviour
+{
+
+    static public ItemGetPopupUI Instance { get; private set; }
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public GameObject ItemBundleUIParent;
+
+    public void ShowItemGet(params ItemBundle[] itemBundles)
+    {
+        foreach (ItemBundle bundle in itemBundles)
+            StartCoroutine(Show(bundle));
+    }
+
+    IEnumerator Show(ItemBundle bundle)
+    {
+        var widget = GetNextItemBundleUI();
+        widget.SetItem(bundle);
+        widget.gameObject.SetActive(true);
+        widget.transform.SetAsFirstSibling();
+        yield return new WaitForSeconds(3f);
+        widget.gameObject.SetActive(false);
+    }
+
+    ItemBundleUI GetNextItemBundleUI()
+    {
+        var list = ItemBundleUIParent.GetComponentsInChildren<ItemBundleUI>(true);
+        return list[list.Length - 1];
+    }
+}
